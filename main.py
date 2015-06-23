20a44e31217dcff1a711de196557b26572b7bb12
Song Young-jin <myevan80@gmail.com>
2015-06-23 21:57:10 +0900

    ! 해상도 설정 코드 정리

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from kivy.app import App
-from distutils.version import StrictVersion
-
-import kivy
+from kivy.config import Config
 
 res_width = 1920 / 4
 res_height = 1080 / 4
 
-if StrictVersion(kivy.__version__) >= StrictVersion('1.9.0'):
-    from kivy.config import Config
-    Config.set('graphics', 'width', str(res_width))
-    Config.set('graphics', 'height', str(res_height))
-else:
-    from kivy.core.window import Window
-    Window.size = (res_width, res_height)
+Config.set('graphics', 'width', str(res_width))
+Config.set('graphics', 'height', str(res_height))
 
 class VegabondsApp(App):
     pass
