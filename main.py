e4332ac23ce101872c0a977efbc16b3aa427df17
jaru <myevan80@gmail.com>
2015-08-19 22:08:16 +0900

    ! 라벨 중앙 출력 + 테두리

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.core.window import Window
 
+from kivy.graphics import *
+
 from random import random 
 
 import json
@@ -25,7 +27,6 @@ class VegabondsView(FloatLayout):
         self.__load_map_scene('china')
 
     def __load_map_scene(self, map_name):
-
         scene_path = os.path.join('resources', 'maps', map_name, 'scene.json')
         scene_dict = json.load(open(scene_path))
 
@@ -34,7 +35,7 @@ class VegabondsView(FloatLayout):
         layer_list = scene_dict['layers']
         first_layer_dict = layer_list[0]
         pivot_pos = (first_layer_dict['x'], first_layer_dict['y'])
-        for layer_dict in reversed(layer_list):
+        for layer_dict in reversed(layer_list[1:]):
             layer_pos = (layer_dict['x'] - pivot_pos[0], layer_dict['y'] - pivot_pos[1])
             layer_size = (layer_dict['w'], layer_dict['h'])
             layer_name = layer_dict['name'] 
@@ -42,10 +43,15 @@ class VegabondsView(FloatLayout):
             
             layer_color = [random(), random(), random(), 1]
             layer_image = Image(source=layer_path, pos=layer_pos, size=layer_size, pos_hint={}, size_hint=(None, None), color=layer_color)
-            
-            layer_label = Label(text="[color=000000]"+layer_name+"[/color]", pos=layer_pos, pos_hint={}, size_hint=(None, None), markup=True)
+	    with layer_image.canvas.before:
+		layer_image.bg_rect = Line(rectangle=(layer_pos[0], layer_pos[1], layer_size[0], layer_size[1]))
             self.add_widget(layer_image)
+            
+            layer_label = Label(text="[color=000000]"+layer_name+"[/color]", pos=layer_pos, size=layer_size, pos_hint={}, size_hint=(None, None), markup=True)
             self.add_widget(layer_label)
+
+	layer_label = Label(text="[color=000000]!!!![/color]", pos=(0, 0), pos_hint={}, size_hint=(None, None), markup=True)
+	self.add_widget(layer_label)
 
 class VegabondsApp(App):
     pass
